0991d14d509982f69f03cae8f2be9f047b0b8e24
Sakib-05 <sakibimamul03@gmail.com>
2025-10-06 10:25:41 +0100

    everyhting is set ofr development

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>its working now</Text>
       <StatusBar style="auto" />
     </View>
   );
